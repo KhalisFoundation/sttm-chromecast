62d8dc6b6c9b6258485fc9e5017a297e3d9fdb0c
Inderpreet Singh <singhs.ishwer@gmail.com>
2021-10-11 11:23:17 +0530

    fix: version 9 data obj compatibilty

--- src/js/index.js
+++ src/js/index.js
@@ -50,104 +50,109 @@ window.onload = () => {
 
 // utility function to display the text message in the input field
 function displayText(json) {
+  console.log('inside display text fn');
   console.log(json);
-  const gurbani = JSON.parse(json);
-  const { prefs } = gurbani;
-  const showNextLine = prefs['slide-layout'].fields['display-next-line'] === true;
-  const showTeeka = prefs['slide-layout'].fields['display-teeka'] === true;
-  const showTranslation = prefs['slide-layout'].fields['display-translation'] === true;
-  let translationLang = 'English';
-  const showtransliteration = prefs['slide-layout'].fields['display-transliteration'] === true;
-  let transliterationLang = 'English';
-  const showVishraams = prefs.toolbar && prefs.toolbar['gurbani-options'] ? prefs.toolbar['gurbani-options']['display-visraams'] === true : false;
-  const gurmukhiElement = document.getElementById('gurmukhi');
-  const larivaarElement = document.getElementById('larivaar');
-  const translationElement = document.getElementById('translation');
-  const teekaElement = document.getElementById('teeka');
-  const transliterationElement = document.getElementById('transliteration');
-  const nextLineElement = document.getElementById('next-line');
-  const slide = document.querySelector('.slide');
+  // document.write(json);
+  // const gurbani = JSON.parse(json);
+  // const { prefs } = gurbani;
+  // const showNextLine = prefs['slide-layout'].fields['display-next-line'] === true;
+  // const showTeeka = prefs['slide-layout'].fields['display-teeka'] === true;
+  // const showTranslation = prefs['slide-layout'].fields['display-translation'] === true;
+  // let translationLang = 'English';
+  // const showtransliteration = prefs['slide-layout'].fields['display-transliteration'] === true;
+  // let transliterationLang = 'English';
+  // const showVishraams = prefs.toolbar && prefs.toolbar['gurbani-options'] ? prefs.toolbar['gurbani-options']['display-visraams'] === true : false;
+  // const gurmukhiElement = document.getElementById('gurmukhi');
+  // const larivaarElement = document.getElementById('larivaar');
+  // const translationElement = document.getElementById('translation');
+  // const teekaElement = document.getElementById('teeka');
+  // const transliterationElement = document.getElementById('transliteration');
+  // const nextLineElement = document.getElementById('next-line');
+  // const slide = document.querySelector('.slide');
 
-  // For backwards compatibility
-  if (prefs.toolbar && prefs.toolbar['language-settings']) {
-    // we are on a version that uses toolbar as place for language settings
-    translationLang = prefs.toolbar['language-settings']['translation-language'];
-    transliterationLang = prefs.toolbar['language-settings']['transliteration-language'];
-  }
+  // // For backwards compatibility
+  // if (prefs.toolbar && prefs.toolbar['language-settings']) {
+  //   // we are on a version that uses toolbar as place for language settings
+  //   translationLang = prefs.toolbar['language-settings']['translation-language'];
+  //   transliterationLang = prefs.toolbar['language-settings']['transliteration-language'];
+  // }
 
-  if (prefs['slide-layout'] && prefs['slide-layout']['language-settings']) {
-    // we are on a version that uses slide-layout as place for language settings
-    translationLang = prefs['slide-layout']['language-settings']['translation-language'];
-    transliterationLang = prefs['slide-layout']['language-settings']['transliteration-language'];
-  }
+  // if (prefs['slide-layout'] && prefs['slide-layout']['language-settings']) {
+  //   // we are on a version that uses slide-layout as place for language settings
+  //   translationLang = prefs['slide-layout']['language-settings']['translation-language'];
+  //   transliterationLang = prefs['slide-layout']['language-settings']['transliteration-language'];
+  // }
 
-  clearClasses();
-  document.body.classList.add(`${prefs.app.theme}`);
+  // clearClasses();
+  // document.body.classList.add(`${prefs.app.theme}`);
 
-  if (!gurbani.showInEnglish) {
-    gurmukhiElement.classList.add('gurmukhi', 'gurbani');
-    larivaarElement.classList.add('gurmukhi', 'gurbani');
-  } else {
-    gurmukhiElement.classList.remove('gurmukhi', 'gurbani');
-    larivaarElement.classList.remove('gurmukhi', 'gurbani');
-  }
+  // if (!gurbani.showInEnglish) {
+  //   gurmukhiElement.classList.add('gurmukhi', 'gurbani');
+  //   larivaarElement.classList.add('gurmukhi', 'gurbani');
+  // } else {
+  //   gurmukhiElement.classList.remove('gurmukhi', 'gurbani');
+  //   larivaarElement.classList.remove('gurmukhi', 'gurbani');
+  // }
 
-  if (showTranslation) document.body.classList.add('display-translation');
-  if (showTeeka) document.body.classList.add('display-teeka');
-  if (showtransliteration) document.body.classList.add('display-transliteration');
-  if (showNextLine) document.body.classList.add('display-next-line');
-  if (showVishraams) {
-    document.body.classList.add('display-visraams');
-    document.body.classList.add(`vishraam-vishraam-options-${prefs.toolbar.vishraam['vishraam-options']}`);
-    document.body.classList.add(`vishraam-vishraam-source-${prefs.toolbar.vishraam['vishraam-source']}`);
-  }
+  // if (showTranslation) document.body.classList.add('display-translation');
+  // if (showTeeka) document.body.classList.add('display-teeka');
+  // if (showtransliteration) document.body.classList.add('display-transliteration');
+  // if (showNextLine) document.body.classList.add('display-next-line');
+  // if (showVishraams) {
+  //   document.body.classList.add('display-visraams');
+  //   document.body.classList.add(`vishraam-vishraam-options-${prefs.toolbar.vishraam['vishraam-options']}`);
+  //   document.body.classList.add(`vishraam-vishraam-source-${prefs.toolbar.vishraam['vishraam-source']}`);
+  // }
 
-  document.body.classList.add(`gurbani-${prefs['slide-layout']['font-sizes'].gurbani}`);
-  document.body.classList.add(`translation-${prefs['slide-layout']['font-sizes'].translation}`);
-  document.body.classList.add(`teeka-${prefs['slide-layout']['font-sizes'].teeka}`);
-  document.body.classList.add(`transliteration-${prefs['slide-layout']['font-sizes'].transliteration}`);
-  document.body.classList.add(`next-line-${prefs['slide-layout']['font-sizes'].gurbani}`);
+  // document.body.classList.add(`gurbani-${prefs['slide-layout']['font-sizes'].gurbani}`);
+  // document.body.classList.add(`translation-${prefs['slide-layout']['font-sizes'].translation}`);
+  // document.body.classList.add(`teeka-${prefs['slide-layout']['font-sizes'].teeka}`);
+  // document.body.classList.add(`transliteration-${prefs['slide-layout']['font-sizes'].transliteration}`);
+  // document.body.classList.add(`next-line-${prefs['slide-layout']['font-sizes'].gurbani}`);
 
-  if (prefs['slide-layout']['display-options'].larivaar === true) {
-    document.body.classList.add('larivaar');
-    if (prefs['slide-layout']['display-options']['larivaar-assist']) {
-      document.body.classList.add('larivaar-assist');
-      const assistType = prefs['slide-layout']['larivaar-settings']['assist-type'];
-      if (assistType) {
-        document.body.classList.add(assistType);
-      }
-    }
-    gurmukhiElement.style.display = 'none';
-    larivaarElement.style.display = 'block';
-  } else {
-    gurmukhiElement.style.display = 'block';
-    larivaarElement.style.display = 'none';
-  }
-  if (prefs['slide-layout']['display-options']['left-align'] === true) document.body.classList.add('left-align');
+  // if (prefs['slide-layout']['display-options'].larivaar === true) {
+  //   document.body.classList.add('larivaar');
+  //   if (prefs['slide-layout']['display-options']['larivaar-assist']) {
+  //     document.body.classList.add('larivaar-assist');
+  //     const assistType = prefs['slide-layout']['larivaar-settings']['assist-type'];
+  //     if (assistType) {
+  //       document.body.classList.add(assistType);
+  //     }
+  //   }
+  //   gurmukhiElement.style.display = 'none';
+  //   larivaarElement.style.display = 'block';
+  // } else {
+  //   gurmukhiElement.style.display = 'block';
+  //   larivaarElement.style.display = 'none';
+  // }
+  // if (prefs['slide-layout']['display-options']['left-align'] === true) document.body.classList.add('left-align');
 
-  gurmukhiElement.innerHTML = gurbani.gurmukhi ? gurbani.gurmukhi : '';
-  larivaarElement.innerHTML = gurbani.larivaar ? `<span class="larivaar">${gurbani.larivaar}</span>` : '';
+  // gurmukhiElement.innerHTML = gurbani.gurmukhi ? gurbani.gurmukhi : '';
+  // larivaarElement.innerHTML = gurbani.larivaar ? `<span class="larivaar">${gurbani.larivaar}</span>` : '';
 
-  if (gurbani.isText) {
-    slide.classList.add('announcement-slide');
-  } else {
-    slide.classList.remove('announcement-slide');
-  }
+  // if (gurbani.isText) {
+  //   slide.classList.add('announcement-slide');
+  // } else {
+  //   slide.classList.remove('announcement-slide');
+  // }
 
-  const translationText = (gurbani && typeof gurbani.translation === 'string') ? gurbani.translation : gurbani.translation[translationLang];
-  const transliterationText = (gurbani && typeof gurbani.transliteration === 'string') ? gurbani.transliteration : gurbani.transliteration[transliterationLang];
+  // const translationText = (gurbani && typeof gurbani.translation === 'string') ? gurbani.translation : gurbani.translation[translationLang];
+  // const transliterationText = (gurbani && typeof gurbani.transliteration === 'string') ? gurbani.transliteration : gurbani.transliteration[transliterationLang];
 
-  translationElement.innerHTML = (gurbani.translation && showTranslation) ? translationText : '';
-  teekaElement.innerHTML = gurbani.teeka && showTeeka ? gurbani.teeka : '';
-  transliterationElement.innerHTML = (gurbani.transliteration && showtransliteration) ? transliterationText : '';
-  nextLineElement.innerHTML = gurbani.nextLine && showNextLine ? gurbani.nextLine : '';
+  // translationElement.innerHTML = (gurbani.translation && showTranslation) ? translationText : '';
+  // teekaElement.innerHTML = gurbani.teeka && showTeeka ? gurbani.teeka : '';
+  // transliterationElement.innerHTML = (gurbani.transliteration && showtransliteration) ? transliterationText : '';
+  // nextLineElement.innerHTML = gurbani.nextLine && showNextLine ? gurbani.nextLine : '';
+
+  document.querySelector('#viewer').innerHTML = `${JSON.parse(json)}`;
 
   // Debug info
-  document.querySelector('.debug-info').innerHTML = `${json}`;
+  // document.querySelector('.debug-info').style.display = 'block';
+  // document.querySelector('.debug-info').innerHTML = `${json}`;
 
-  window.castReceiverManager.setApplicationState(json);
+  // window.castReceiverManager.setApplicationState(json);
 }
 
-function clearClasses() {
-  document.body.className = '';
-}
+// function clearClasses() {
+//   document.body.className = '';
+// }
